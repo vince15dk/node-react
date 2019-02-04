d168614c8f48c39fb41460fbc15367615945ac3a
SJ <vince15dk@gmail.com>
2019-02-04 23:05:25 +0900

    config security added

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+const {googleClientID, googleClientSecret} = require('./config/keys');
 const app = express();
 
-app.get('/', (req, res)=>{
-    res.send({
-        bye: 'dude'
-    });
-})
+passport.use(new GoogleStrategy());
 
-
+//https://console.developers.google.com
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, ()=>{
@@ -16,3 +15,9 @@ app.listen(PORT, ()=>{
 })
 
 
+
+// Since the main goal of using http://localhost:5000/* was to show the redirect error a few lectures later,  we can do one of two things here:
+
+// 1. Leave the authorized URI Redirect blank, since we will be fixing this later.
+
+// 2. Enter http://localhost:5000/auth/google/callback now, since that is what it will be changed to in a later lecture.
