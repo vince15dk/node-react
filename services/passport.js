4cc6223950d08c32ecd539ff216227eebcc12d24
SJ <vince15dk@gmail.com>
2019-02-07 15:51:21 +0900

    tweat google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -22,7 +22,8 @@ passport.use(
     new GoogleStrategy({
     clientID: googleClientID,
     clientSecret: googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
 },async (accessToken, refreshToken, profile, done)=>{
     try{
         const user = await User.findOne({googleId: profile.id})
